18b18bce15c463d283f8f6ba16db4fca4c1b6cd8
Alec Hannaford <alec322@gmail.com>
2023-07-14 15:23:33 -0700

    console logs the search result sucessfully

--- src/App.js
+++ src/App.js
@@ -2,21 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import SearchBar from './Components/SearchBar';
 import { useState } from 'react';
-
+import { autosuggest } from './Autosuggest/autosuggest';
 
 function App() {
 
 
 
-  //change handler
 
 
   const [resultState, serResultState] = useState('');
   const [searchState, setSearchState] = useState('');
-
+  
+  //change handler
   async function searchbarChangeHandler(event){
-    console.log("changed!");
+    console.log("Event target value ", event.target.value);
     setSearchState(event.target.value);
+    console.log(await autosuggest(event))
   }
 
   return (
